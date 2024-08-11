a214c46472def09f1b38f14bad67562af0053dc6
Aryan Malik <aryanhunny98@gmail.com>
2024-08-11 10:58:22 +0530

    Attestation, ID and SuperHunt contract deployed on Optimism Sepolia

--- ignition/modules/Deploy_OP.js
+++ ignition/modules/Deploy_OP.js
@@ -1,4 +1,10 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+const { vars } = require("hardhat/config");
+
+const APP_ID = vars.get("APP_ID");
+const ACTION_ID = vars.get("ACTION_ID");
+const PYTH_OP_SEPOLIA= '0x0708325268dF9F66270F1401206434524814508b'
+const ETH_USD_ID='0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace'
 
 const OP_EAS_CONTRACT = "0x4200000000000000000000000000000000000021"
 
@@ -6,7 +12,9 @@ module.exports = buildModule("Deploy_OP", (m) => {
 
   const attestation = m.contract("Attestation", [OP_EAS_CONTRACT]);
 
-  const id = m.contract("ID", )
+  const id = m.contract("ID", [APP_ID, ACTION_ID]);
 
-  return { attestation };
+  const superhuntOP = m.contract("SuperHuntOP", [PYTH_OP_SEPOLIA, ETH_USD_ID]);
+
+  return { attestation, id, superhuntOP };
 });
